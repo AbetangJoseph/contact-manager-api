3762b278224273e75571f542d2c7692c328b3bbc
Abetang Joseph <josephabetang@gmail.com>
2019-07-11 22:07:49 +0100

    modify .send to .json

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,5 +2,5 @@ import { Request, Response } from 'express'
 import { contactList } from '../models/contacts'
 
 export const getContactsController = (_req: Request, res: Response) => {
-  res.status(200).send(contactList)
+  res.status(200).json({ users: contactList })
 }
