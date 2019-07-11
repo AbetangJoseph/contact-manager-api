6e04f5926f0652829de5099608499c294c01e57a
Abetang Joseph <josephabetang@gmail.com>
2019-07-11 22:34:34 +0100

    add getContactController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,3 +4,8 @@ import { contactList } from '../models/contacts'
 export const getContactsController = (_req: Request, res: Response) => {
   res.status(200).json({ users: contactList })
 }
+
+export const getContactController = (req: Request, res: Response) => {
+  const user = contactList.find(user => user.id === parseInt(req.params.id))
+  res.status(200).json({ user: user })
+}
