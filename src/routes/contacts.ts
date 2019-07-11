96b20537e9b3e29301d366b4ff0a7553a2296292
Abetang Joseph <josephabetang@gmail.com>
2019-07-11 15:53:48 +0100

    add routes

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { getContactsController } from '../controllers'
 
 const router = Router()
+
+router.get('/contacts', getContactsController)
 
 export default router
