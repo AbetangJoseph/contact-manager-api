f4e2372281a3e55500a595c97b4f38b37146abdc
Abetang Joseph <josephabetang@gmail.com>
2019-07-11 17:38:18 +0100

    modify getContactsController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,6 @@
-// export const create = (_req, res) => {
-//   res.status(200).json({ message: 'hello backend boy' })
-// }
 import { Request, Response } from 'express'
+import { contactList } from '../models/contacts'
 
 export const getContactsController = (_req: Request, res: Response) => {
-  res.status(200).json({ message: 'hello backend boss' })
+  res.status(200).send(contactList)
 }
