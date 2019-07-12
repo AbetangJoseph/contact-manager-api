e062015806d8e26868efa0e568588719c4942366
Abetang Joseph <josephabetang@gmail.com>
2019-07-12 03:05:18 +0100

    add deleteContactController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,37 +1,51 @@
-import { Request, Response } from 'express'
-import { contactList } from '../models/contacts'
-import { validateInput } from '../middleware/validator'
-import { dateCreated } from '../helpers/date'
-import { userSchema } from '../utils/validation-schema'
+import { Request, Response } from 'express';
+import { contactList } from '../models/contacts';
+import { validateInput } from '../middleware/validator';
+import { dateCreated } from '../helpers/date';
+import { userSchema } from '../utils/validation-schema';
 
 export const getContactsController = (_req: Request, res: Response) => {
-  res.status(200).json({ users: contactList })
-}
+  res.status(200).json({ users: contactList });
+};
 
 export const getContactController = (req: Request, res: Response) => {
-  const user = contactList.find(user => user.id === parseInt(req.params.id))
+  const user = contactList.find(user => user.id === parseInt(req.params.id));
   if (!user) {
-    res.json({ message: 'no such user' })
-    return
+    res.json({ message: 'no such user' });
+    return;
   }
-  res.status(200).json({ user: user })
-}
+  res.status(200).json({ user: user });
+};
 
 export const postContactController = (req: Request, res: Response) => {
-  const { error, value } = validateInput(req.body, userSchema)
+  const { error, value } = validateInput(req.body, userSchema);
 
   if (error) {
-    res.status(400).json({ message: error.message })
-    return
+    res.status(400).json({ message: error.message });
+    return;
   }
 
-  const userId = contactList.length + 1
-  const date = dateCreated()
-  const user = { ...value, id: userId, created: date }
-  contactList.push(user)
+  const userId = contactList.length + 1;
+  const date = dateCreated();
+  const user = { ...value, id: userId, created: date };
+  contactList.push(user);
   res.status(200).json({
     status: 'success',
     message: 'User created successfully',
     data: { ...user },
-  })
-}
+  });
+};
+
+export const deleteContactController = (req: Request, res: Response) => {
+  const userIndex = contactList.findIndex(
+    user => user.id === parseInt(req.params.id),
+  );
+
+  if (userIndex === -1) {
+    res.status(404).json({ message: 'no such user' });
+    return;
+  }
+
+  contactList.splice(userIndex, 1);
+  res.status(200).json({ message: 'User deleted successfully' });
+};
