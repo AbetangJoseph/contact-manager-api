e447f3feceadf85a8cb86bc0c2f975d7b76eb375
Abetang Joseph <josephabetang@gmail.com>
2019-07-15 04:43:24 +0100

    import interface definition

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,19 +1,18 @@
-import joi from '@hapi/joi'
+import joi from '@hapi/joi';
+import { addUser, editUser } from '../interface.def';
 
-interface User {
-  firstname: string
-  lastname?: string
-  mobile: string
-  address?: string
-  email?: string
-  company?: string
-  website?: string
-}
-
-export const validateInput = (requestBody: any, schema: any) => {
-  const { error, value } = joi.validate<User>(requestBody, schema, {
+export const validateAdd = (requestBody: any, schema: any) => {
+  const { error, value } = joi.validate<addUser>(requestBody, schema, {
     abortEarly: false,
     stripUnknown: true,
-  })
-  return { error, value }
-}
+  });
+  return { error, value };
+};
+
+export const validateUpdate = (requestBody: any, schema: any) => {
+  const { error, value } = joi.validate<editUser>(requestBody, schema, {
+    abortEarly: false,
+    stripUnknown: true,
+  });
+  return { error, value };
+};
